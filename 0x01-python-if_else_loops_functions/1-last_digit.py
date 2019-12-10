e093c4402694a2cd12b78f6605e06440394c8a75
andreammgcol <amendez72@misena.edu.co>
2019-12-10 16:53:56 -0500

    task 1 last digit correct2

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 last = abs(number) % 10
 if number < 10:
     last = last * -1
-if last > 5:
+if last > 5 and last != 0:
     print('Last digit of {} is {} and is greater than 5'. format(number, last))
 elif last < 6 and last !=0:
     print('Last digit of {} is {} and is less than 6 and not 0'. format(number, last))
