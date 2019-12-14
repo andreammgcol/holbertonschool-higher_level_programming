38bee11c8d09cad9782f0b9451c579be4534c55b
andreammgcol <amendez72@misena.edu.co>
2019-12-13 19:44:10 -0500

    task 6 my own calculator fixed

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -11,13 +11,13 @@ if __name__ == "__main__":
         a = int(sys.argv[1])
         b = int(sys.argv[3])
         if sys.argv[2] == '+':
-            print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+            print('{} + {} = {}'.format(a, b, add(a, b)))
         elif sys.argv[2] == '-':
-            print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
-        elif sys.argv[2] == '*':
-            print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
+            print('{} - {} = {}'.format(a, b, sub(a, b)))
+        elif sys.argv[2] == "*":
+            print('{} * {} = {}'.format(a, b, mul(a, b)))
         elif sys.argv[2] == '/':
-                print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
+                print('{} / {} = {}'.format(a, b, div(a, b)))
         else:
             print('Unknown operator. Available operators: +, -, * and /')
             sys.exit(1)
