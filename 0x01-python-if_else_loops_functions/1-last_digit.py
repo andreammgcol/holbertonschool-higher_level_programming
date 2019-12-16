14729f058c4d2e6e80242f96dbe1fa19a5478c4d
andreammgcol <amendez72@misena.edu.co>
2019-12-15 19:02:23 -0500

    fix PEP8 validation

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,6 +8,6 @@ if last > 5:
     print('Last digit of {} is {} and is greater than 5'. format(number, last))
 elif last < 6 and last != 0:
     print('Last digit of {} is {} and is less than 6 and not 0'.
-     format(number, last))
+    format(number, last))
 else:
     print('Last digit of {} is {} and is 0'. format(number, last))
