5dd88e319b9f7d9adbbc55f6306689d2fd87aed7
andreammgcol <amendez72@misena.edu.co>
2019-12-15 19:43:20 -0500

    fix some things

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-	if i == 101 or i == 113:
-		continue
-	else:
-		print(chr(i), end='')
+i = 0
+while i < 26: 
+    if i != 4 and i != 16:   
+        print('{:c}'. format(i + 97), end='')
+    i = i + 1
