3f50b00acafa06401b90d3fd11add9a80e211f71
andreammgcol <amendez72@misena.edu.co>
2019-12-20 00:12:42 -0500

    task 12 roman to int fix req

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string:
+    if roman_string is None:
+        return 0
+    if roman_string == "":
         return 0
     romans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     number = len(roman_string)
