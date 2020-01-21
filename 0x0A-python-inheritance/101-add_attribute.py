8cc3702f2523be1a13b7e1ea74d2393518fd610c
andreammgcol <amendez72@misena.edu.co>
2020-01-20 22:57:34 -0500

    task 101 advanced

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+''' add_attribute function ''''
+
+
+def add_attribute(obj, name, value):
+    ''' function that adds a new attribute
+        to an object if it’s possible
+    '''
+    if isinstance(obj, (int, float, str, tuple, bool, dict, frozenset)):
+        raise TypeError("can't add new attribute")
+    else:
+        setattr(obj, name, value)
