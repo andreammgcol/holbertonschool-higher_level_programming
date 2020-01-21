8371f7a74a64f8fc897f3d6e375c917e478ccbdc
andreammgcol <amendez72@misena.edu.co>
2020-01-20 20:16:57 -0500

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+BaseGeometry module
+'''
+
+
+class BaseGeometry:
+    ''' class BaseGeometry '''
+
+    def area(self):
+        ''' Public instance method area '''
+        raise Exception("area() is not implemented")
