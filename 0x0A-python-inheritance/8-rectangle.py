593b4772dafc2b0792f65e2d642d4215ab374b7d
andreammgcol <amendez72@misena.edu.co>
2020-01-20 20:33:50 -0500

    task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+BaseGeometry module
+'''
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    ''' class Rectangle '''
+
+    def __init__(self, width, height):
+        '''
+        Instantiation with width and height
+        Arguments:
+        @width: width size
+        @height: height size
+        '''
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
