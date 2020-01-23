ef0a97699571133ebd3ff1749fac8f6b0a4245b9
andreammgcol <amendez72@misena.edu.co>
2020-01-22 20:12:02 -0500

    task 4 fixed

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -3,5 +3,5 @@ def append_write(filename="", text=""):
     ''' that appends a string at the end of a text file
         (UTF8) and returns the number of characters added
     '''
-    with open("filename", mode='a', encoding='utf-8') as f:
+    with open(filename, mode='a', encoding='utf-8') as f:
         return f.write(text)
