61ffcaf7b795aaf5bb8cbb6e714231ab0e2f9a92
andreammgcol <amendez72@misena.edu.co>
2020-01-27 19:31:27 -0500

    fix pep8

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -10,162 +10,162 @@ from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
 
+
 class Testrectangle(unittest.TestCase):
-	"""
+    """
     Test cases for the Rectangle class
     """
 
-	def test_pep8_conformance_rectangle(self):
-		"""Test that we conform to PEP8."""
-		pep8style = pep8.StyleGuide(quiet=True)
-		result = pep8style.check_files(['models/rectangle.py'])
-		self.assertEqual(result.total_errors, 0,
-				"Found code style errors (and warnings).")
+    def test_pep8_conformance_rectangle(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/rectangle.py'])
+        self.assertEqual(result.total_errors, 0, "Found code style errors")
 
-	def set_Up(self):
-		"""
-		Redirect the stdout to check the output
-		"""
-		sys.stdout = StringIO()
+    def set_Up(self):
+        """
+        Redirect the stdout to check the output
+        """
+        sys.stdout = StringIO()
 
-	def reset_clean(self):
-		"""
-		Reset the nb_objects and clean everything
-		"""
-		Base._Base__nb_objects = 0
-		sys.stdout = sys.__stdout__
+    def reset_clean(self):
+        """
+        Reset the nb_objects and clean everything
+        """
+        Base._Base__nb_objects = 0
+        sys.stdout = sys.__stdout__
 
-	def test_subclass(self):
-		self.assertTrue(issubclass(Rectangle, Base))
+    def test_subclass(self):
+        self.assertTrue(issubclass(Rectangle, Base))
 
-	def test_docstring(self):
-		"""
-		Test docstring
-		"""
-		self.assertIsNotNone(Rectangle.__doc__)
+    def test_docstring(self):
+        """
+        Test docstring
+        """
+        self.assertIsNotNone(Rectangle.__doc__)
 
-	def test_documentation(self):
-		"""
-		Test documentation is correct and created
-		"""
-		self.assertTrue(hasattr(Rectangle, "__init__"))
-		self.assertTrue(Rectangle.__init__.__doc__)
-		self.assertTrue(hasattr(Rectangle, "create"))
-		self.assertTrue(Rectangle.create.__doc__)
-		self.assertTrue(hasattr(Rectangle, "to_json_string"))
-		self.assertTrue(Rectangle.to_json_string.__doc__)
-		self.assertTrue(hasattr(Rectangle, "from_json_string"))
-		self.assertTrue(Rectangle.from_json_string.__doc__)
-		self.assertTrue(hasattr(Rectangle, "save_to_file"))
-		self.assertTrue(Rectangle.save_to_file.__doc__)
-		self.assertTrue(hasattr(Rectangle, "load_from_file"))
-		self.assertTrue(Rectangle.load_from_file.__doc__)
+    def test_documentation(self):
+        """
+        Test documentation is correct and created
+        """
+        self.assertTrue(hasattr(Rectangle, "__init__"))
+        self.assertTrue(Rectangle.__init__.__doc__)
+        self.assertTrue(hasattr(Rectangle, "create"))
+        self.assertTrue(Rectangle.create.__doc__)
+        self.assertTrue(hasattr(Rectangle, "to_json_string"))
+        self.assertTrue(Rectangle.to_json_string.__doc__)
+        self.assertTrue(hasattr(Rectangle, "from_json_string"))
+        self.assertTrue(Rectangle.from_json_string.__doc__)
+        self.assertTrue(hasattr(Rectangle, "save_to_file"))
+        self.assertTrue(Rectangle.save_to_file.__doc__)
+        self.assertTrue(hasattr(Rectangle, "load_from_file"))
+        self.assertTrue(Rectangle.load_from_file.__doc__)
 
-	def test_parameters(self):
+    def test_parameters(self):
 
-		r1 = Rectangle(10, 2)
-		r2 = Rectangle(2, 10)
-		r3 = Rectangle(10, 2, 0, 0, 12)
+        r1 = Rectangle(10, 2)
+        r2 = Rectangle(2, 10)
+        r3 = Rectangle(10, 2, 0, 0, 12)
 
-		self.assertEqual(r1.id, 1)
-		self.assertEqual(r1.width, 10)
-		self.assertEqual(r1.height, 2)
-		self.assertEqual(r1.x, 0)
-		self.assertEqual(r1.y, 0)
+        self.assertEqual(r1.id, 1)
+        self.assertEqual(r1.width, 10)
+        self.assertEqual(r1.height, 2)
+        self.assertEqual(r1.x, 0)
+        self.assertEqual(r1.y, 0)
 
-		self.assertEqual(r2.id, 2)
-		self.assertEqual(r2.width, 2)
-		self.assertEqual(r2.height, 10)
-		self.assertEqual(r2.x, 0)
-		self.assertEqual(r2.y, 0)
+        self.assertEqual(r2.id, 2)
+        self.assertEqual(r2.width, 2)
+        self.assertEqual(r2.height, 10)
+        self.assertEqual(r2.x, 0)
+        self.assertEqual(r2.y, 0)
 
-		self.assertEqual(r3.id, 12)
-		self.assertEqual(r3.width, 10)
-		self.assertEqual(r3.height, 2)
-		self.assertEqual(r3.x, 0)
-		self.assertEqual(r3.y, 0)
+        self.assertEqual(r3.id, 12)
+        self.assertEqual(r3.width, 10)
+        self.assertEqual(r3.height, 2)
+        self.assertEqual(r3.x, 0)
+        self.assertEqual(r3.y, 0)
 
-		with self.assertRaises(TypeError):
-			r4 = Rectangle()
+        with self.assertRaises(TypeError):
+            r4 = Rectangle()
 
-	def is_string(self):
-		with self.assertRaises(TypeError):
-			R1 = Rectangle(1.01, 3)
-		with self.assertRaises(TypeError):
-			R1 = Rectangle(1.01, 3)
+    def is_string(self):
+        with self.assertRaises(TypeError):
+            R1 = Rectangle(1.01, 3)
+        with self.assertRaises(TypeError):
+            R1 = Rectangle(1.01, 3)
 
-	def test_type_param(self):
-		""" width instance testing """
-		with self.assertRaises(TypeError):
-			R1 = Rectangle(1.01, 3)
-			raise TypeError()
-		with self.assertRaises(ValueError):
-			R2 = Rectangle(-234234242, 45)
-			raise ValueError()
-		with self.assertRaises(TypeError):
-			R3 = Rectangle("", 4)
-			raise TypeError()
-		with self.assertRaises(TypeError):
-			R4 = Rectangle(True, 4)
-			raise TypeError()
+    def test_type_param(self):
+        """ width instance testing """
+        with self.assertRaises(TypeError):
+            R1 = Rectangle(1.01, 3)
+            raise TypeError()
+        with self.assertRaises(ValueError):
+            R2 = Rectangle(-234234242, 45)
+            raise ValueError()
+        with self.assertRaises(TypeError):
+            R3 = Rectangle("", 4)
+            raise TypeError()
+        with self.assertRaises(TypeError):
+            R4 = Rectangle(True, 4)
+            raise TypeError()
 
-		""" height instance testing """
-		with self.assertRaises(TypeError):
-			H1 = Rectangle(5, 1.76)
-			raise TypeError()
-		with self.assertRaises(TypeError):
-			H2 = Rectangle(5, "Hello")
-			raise TypeError()
-		with self.assertRaises(TypeError):
-			H3 = Rectangle(5, False)
-			raise TypeError()
-		with self.assertRaises(ValueError):
-			H1 = Rectangle(5, -4798576398576)
-			raise ValueError()
+        """ height instance testing """
+        with self.assertRaises(TypeError):
+            H1 = Rectangle(5, 1.76)
+            raise TypeError()
+        with self.assertRaises(TypeError):
+            H2 = Rectangle(5, "Hello")
+            raise TypeError()
+        with self.assertRaises(TypeError):
+            H3 = Rectangle(5, False)
+            raise TypeError()
+        with self.assertRaises(ValueError):
+            H1 = Rectangle(5, -4798576398576)
+            raise ValueError()
 
-		""" x instance testing """
-		with self.assertRaises(TypeError):
-			H1 = Rectangle(5, 1, 1.50)
-			raise TypeError()
-		with self.assertRaises(TypeError):
-			H2 = Rectangle(5, 6, "test")
-			raise TypeError()
-		with self.assertRaises(TypeError):
-			H3 = Rectangle(5, 7, False)
-			raise TypeError()
-		with self.assertRaises(ValueError):
-			H1 = Rectangle(5, 7, -4798576398576)
-			raise ValueError()
+        """ x instance testing """
+        with self.assertRaises(TypeError):
+            H1 = Rectangle(5, 1, 1.50)
+            raise TypeError()
+        with self.assertRaises(TypeError):
+            H2 = Rectangle(5, 6, "test")
+            raise TypeError()
+        with self.assertRaises(TypeError):
+            H3 = Rectangle(5, 7, False)
+            raise TypeError()
+        with self.assertRaises(ValueError):
+            H1 = Rectangle(5, 7, -4798576398576)
+            raise ValueError()
 
-		""" y instance testing """
-		with self.assertRaises(TypeError):
-			H1 = Rectangle(5, 1, 1, 1.53)
-			raise TypeError()
-		with self.assertRaises(TypeError):
-			H2 = Rectangle(5, 6, 5, "test")
-			raise TypeError()
-		with self.assertRaises(TypeError):
-			H3 = Rectangle(5, 7, 7, False)
-			raise TypeError()
-		with self.assertRaises(ValueError):
-			H1 = Rectangle(5, 9, 5, -4798576398576)
-			raise ValueError()
+        """ y instance testing """
+        with self.assertRaises(TypeError):
+            H1 = Rectangle(5, 1, 1, 1.53)
+            raise TypeError()
+        with self.assertRaises(TypeError):
+            H2 = Rectangle(5, 6, 5, "test")
+            raise TypeError()
+        with self.assertRaises(TypeError):
+            H3 = Rectangle(5, 7, 7, False)
+            raise TypeError()
+        with self.assertRaises(ValueError):
+            H1 = Rectangle(5, 9, 5, -4798576398576)
+            raise ValueError()
 
-	def test_rectangle_area(self):
-		"""
-		Test area method
-		"""
-		r = Rectangle(1, 2)
-		r1 = Rectangle(3, 4, 5)
-		r2 = Rectangle(6, 7, 8, 9)
-		r3 = Rectangle(10, 11, 12, 13, 14)
-		r4 = Rectangle(9999999999999, 9999999999999)
-		self.assertEqual(r.area(), 2)
-		self.assertEqual(r1.area(), 3 * 4)
-		self.assertEqual(r2.area(), 6 * 7)
-		self.assertEqual(r3.area(), 10 * 11)
-		self.assertEqual(r4.area(), 9999999999999 * 9999999999999)
+    def test_rectangle_area(self):
+        """
+        Test area method
+        """
+        r = Rectangle(1, 2)
+        r1 = Rectangle(3, 4, 5)
+        r2 = Rectangle(6, 7, 8, 9)
+        r3 = Rectangle(10, 11, 12, 13, 14)
+        r4 = Rectangle(9999999999999, 9999999999999)
+        self.assertEqual(r.area(), 2)
+        self.assertEqual(r1.area(), 3 * 4)
+        self.assertEqual(r2.area(), 6 * 7)
+        self.assertEqual(r3.area(), 10 * 11)
+        self.assertEqual(r4.area(), 9999999999999 * 9999999999999)
 
 
 if __name__ == "_main_":
-	unittest.main()
+    unittest.main()
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -12,134 +12,133 @@ from models.square import Square
 
 
 class TestSquare(unittest.TestCase):
-	"""
+    """
     Test cases for the Square class
     """
-	
-	def test_pep8_conformance_square(self):
-		"""Test that we conform to PEP8."""
-		pep8style = pep8.StyleGuide(quiet=True)
-		result = pep8style.check_files(['models/square.py'])
-		self.assertEqual(result.total_errors, 0,
-						"Found code style errors (and warnings).")
 
-	def test_getter(self):
-		r1 = Square(5)
-		self.assertEqual(r1.size, 5)
+    def test_pep8_conformance_square(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/square.py'])
+        self.assertEqual(result.total_errors, 0, "Found code style errors")
 
-	def test_setter(self):
-		r1 = Square(5)
-		r1.size = 8
-		self.assertEqual(r1.size, 8)
+    def test_getter(self):
+        r1 = Square(5)
+        self.assertEqual(r1.size, 5)
 
-	def test_string(self):
-		r1 = Square(3)
-		with self.assertRaises(TypeError):
-			r1.size = "Hi"
+    def test_setter(self):
+        r1 = Square(5)
+        r1.size = 8
+        self.assertEqual(r1.size, 8)
 
-	def test_negative(self):
-		r1 = Square(6)
-		with self.assertRaises(ValueError):
-			r1.size = -5
+    def test_string(self):
+        r1 = Square(3)
+        with self.assertRaises(TypeError):
+            r1.size = "Hi"
 
-	def test_zero(self):
-		r1 = Square(6)
-		with self.assertRaises(ValueError):
-			r1.size = 0
+    def test_negative(self):
+        r1 = Square(6)
+        with self.assertRaises(ValueError):
+            r1.size = -5
 
-	def test_zero(self):
-		r1 = Square(6)
-		with self.assertRaises(TypeError):
-			r1.size = 1.5
+    def test_zero(self):
+        r1 = Square(6)
+        with self.assertRaises(ValueError):
+            r1.size = 0
 
-	def test_tupla(self):
-		r1 = Square(7)
-		with self.assertRaises(TypeError):
-			r1.size = (2, 8)
+    def test_zero(self):
+        r1 = Square(6)
+        with self.assertRaises(TypeError):
+            r1.size = 1.5
 
-	def test_empty(self):
-		r1 = Square(7)
-		with self.assertRaises(TypeError):
-			r1.size = ""
+    def test_tupla(self):
+        r1 = Square(7)
+        with self.assertRaises(TypeError):
+            r1.size = (2, 8)
 
-	def test_none(self):
-		r1 = Square(5)
-		with self.assertRaises(TypeError):
-			r1.size = None
+    def test_empty(self):
+        r1 = Square(7)
+        with self.assertRaises(TypeError):
+            r1.size = ""
 
-	def test_list(self):
-		r1 = Square(4)
-		with self.assertRaises(TypeError):
-			r1.size = [4, 7]
+    def test_none(self):
+        r1 = Square(5)
+        with self.assertRaises(TypeError):
+            r1.size = None
 
-	def test_dict(self):
-		r1 = Square(5)
-		with self.assertRaises(TypeError):
-			r1.size = {"hi": 5, "world": 8}
+    def test_list(self):
+        r1 = Square(4)
+        with self.assertRaises(TypeError):
+            r1.size = [4, 7]
 
-	def test_width(self):
-		r1 = Square(5)
-		r1.size = 6
-		self.assertEqual(r1.width, 6)
-		self.assertEqual(r1.height, 6)
+    def test_dict(self):
+        r1 = Square(5)
+        with self.assertRaises(TypeError):
+            r1.size = {"hi": 5, "world": 8}
 
-	def test_id(self):
-		S1 = Square(1)
-		S2 = Square(1, 2)
-		S3 = Square(1, 2, 3)
-		S4 = Square(1, 2, 3, 4)
-		self.assertEqual(S2.x, 2)
-		self.assertEqual(S3.y, 3)
-		self.assertEqual(S4.id, 4)
+    def test_width(self):
+        r1 = Square(5)
+        r1.size = 6
+        self.assertEqual(r1.width, 6)
+        self.assertEqual(r1.height, 6)
 
-	def test_ValueError(self):
-		Base._Base__nb_objects = 0
-		with self.assertRaisesRegex(ValueError, "width must be > 0"):
-			Square(-2)
-			Square(0)
-		with self.assertRaisesRegex(ValueError, "x must be >= 0"):
-			Square(10, -3)
-		with self.assertRaisesRegex(ValueError, "y must be >= 0"):
-			Square(10, 14, -5)
+    def test_id(self):
+        S1 = Square(1)
+        S2 = Square(1, 2)
+        S3 = Square(1, 2, 3)
+        S4 = Square(1, 2, 3, 4)
+        self.assertEqual(S2.x, 2)
+        self.assertEqual(S3.y, 3)
+        self.assertEqual(S4.id, 4)
 
-	def test_TypeError(self):
-		Base._Base__nb_objects = 0
-		with self.assertRaisesRegex(TypeError, "width must be an integer"):
-			Square("22")
-			Square(True)
-		with self.assertRaisesRegex(TypeError, "x must be an integer"):
-			Square(10, "yup")
-			Square(10, True)
-		with self.assertRaisesRegex(TypeError, "y must be an integer"):
-			Square(10, 11, "hi")
-			Square(10, 15, False)
+    def test_ValueError(self):
+        Base._Base__nb_objects = 0
+        with self.assertRaisesRegex(ValueError, "width must be > 0"):
+            Square(-2)
+            Square(0)
+        with self.assertRaisesRegex(ValueError, "x must be >= 0"):
+            Square(10, -3)
+        with self.assertRaisesRegex(ValueError, "y must be >= 0"):
+            Square(10, 14, -5)
 
-	def test_str(self):
-		""" test str method """
-		Base._Base__nb_objects = 0
-		S1 = Square(3)
-		S2 = Square(3, 4)
-		S3 = Square(3, 4, 5)
-		self.assertEqual(S1.__str__(), "[Square] (1) 0/0 - 3")
-		self.assertEqual(S2.__str__(), "[Square] (2) 4/0 - 3")
-		self.assertEqual(S3.__str__(), "[Square] (3) 4/5 - 3")
+    def test_TypeError(self):
+        Base._Base__nb_objects = 0
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Square("22")
+            Square(True)
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            Square(10, "yup")
+            Square(10, True)
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Square(10, 11, "hi")
+            Square(10, 15, False)
 
-	def test_dictionary(self):
-		""" test dictionary """
-		re1 = Rectangle(10, 7, 2, 8, 70)
-		dictionary = re1.to_dictionary()
-		json_dictionary = Base.to_json_string([dictionary])
-		self.assertEqual(type(json_dictionary), str)
+    def test_str(self):
+        """ test str method """
+        Base._Base__nb_objects = 0
+        S1 = Square(3)
+        S2 = Square(3, 4)
+        S3 = Square(3, 4, 5)
+        self.assertEqual(S1.__str__(), "[Square] (1) 0/0 - 3")
+        self.assertEqual(S2.__str__(), "[Square] (2) 4/0 - 3")
+        self.assertEqual(S3.__str__(), "[Square] (3) 4/5 - 3")
 
-	def test_dictionary_empty(self):
-		""" test dictionary empty list"""
-		dictionary = []
-		json_dictionary = Base.to_json_string(dictionary)
-		self.assertEqual(json_dictionary, "[]")
+    def test_dictionary(self):
+        """ test dictionary """
+        re1 = Rectangle(10, 7, 2, 8, 70)
+        dictionary = re1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        self.assertEqual(type(json_dictionary), str)
 
-		obj = None
-		json_dictionary = Base.to_json_string(obj)
-		self.assertEqual(json_dictionary, "[]")
+    def test_dictionary_empty(self):
+        """ test dictionary empty list"""
+        dictionary = []
+        json_dictionary = Base.to_json_string(dictionary)
+        self.assertEqual(json_dictionary, "[]")
+
+        obj = None
+        json_dictionary = Base.to_json_string(obj)
+        self.assertEqual(json_dictionary, "[]")
 
 if __name__ == "_main_":
-   unittest.main()
+    unittest.main()
