567180e475b23da481b65d0721fc3857bff05322
andreammgcol <amendez72@misena.edu.co>
2020-03-03 14:42:12 -0500

    create database and user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- script that creates the database hbtn_0d_2
+-- and the user user_0d_2
+CREATE database IF NOT EXISTS hbtn_0d_2; 
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+GRANT ALL PRIVILEGES ON * . * TO user_0d_2@localhost;
