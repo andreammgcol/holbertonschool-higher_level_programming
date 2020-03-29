a2dac6e42fd9be73081051e6adad251a3add7508
andreammgcol <amendez72@misena.edu.co>
2020-03-29 18:29:34 -0500

    fixing permissions

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     mydb = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3])
     cur = mydb.cursor()
-    cur.execute("""SELECT * from states WHERE name = %s ORDER BY id ASC""",
+    cur.execute("SELECT * from states WHERE name = %s ORDER BY id ASC",
                 (argv[4],))
     states = cur.fetchall()
     for records in states:
