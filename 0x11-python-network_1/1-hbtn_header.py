89b8cb68165286c2c7ba7dcd139c1315a3410d56
andreammgcol <amendez72@misena.edu.co>
2020-04-16 18:34:02 +0000

    remove blank line pep8

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -12,4 +12,3 @@ if __name__ == "__main__":
 
     with urllib.request.urlopen(sys.argv[1]) as response:
         print(response.getheader('X-Request-Id'))
-
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -19,4 +19,3 @@ if __name__ == "__main__":
     with request.urlopen(req) as response:
         html = response.read()
     print(html.decode(encoding="utf-8"))
-
