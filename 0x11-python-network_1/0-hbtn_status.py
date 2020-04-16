dd0dc004accfc918a9f2aa3698c2152a32ef19c2
andreammgcol <amendez72@misena.edu.co>
2020-04-16 01:04:01 +0000

    fixing pep8

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-""" script that fetches https://intranet.hbtn.io/status """
+"""
+    script that fetches https://intranet.hbtn.io/status
+"""
+
 import urllib.request
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
