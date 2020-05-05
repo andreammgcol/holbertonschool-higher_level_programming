282250cac701fc59cb131246373436c809c31cda
andreammgcol <amendez72@misena.edu.co>
2020-05-05 18:18:51 -0500

    remove length

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-// prints the first argument passed to it
-if (process.argv.length > 2) {
+if (process.argv[2]) {
   console.log(process.argv[2]);
 } else {
   console.log('No argument');
