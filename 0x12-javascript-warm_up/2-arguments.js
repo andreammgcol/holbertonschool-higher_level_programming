be13271a7af26372668eafcb888b32ed0be9018c
andreammgcol <amendez72@misena.edu.co>
2020-05-05 17:56:04 -0500

    fix semistandard

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-let myArgs = process.argv.slice(1);
+const myArgs = process.argv.slice(1);
 if (myArgs.length < 2) {
-    console.log('No argument');
+  console.log('No argument');
 } else {
-    console.log('Argument found')
+  console.log('Argument found');
 }
