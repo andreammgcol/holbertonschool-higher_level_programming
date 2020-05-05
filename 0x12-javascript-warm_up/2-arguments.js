3717e540e53e1268523440c2c93a97c43cd5a123
andreammgcol <amendez72@misena.edu.co>
2020-05-05 18:11:29 -0500

    adding arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 const myArgs = process.argv.slice(1);
-if (myArgs.length < 2) {
-  console.log('No argument');
-} else {
+if (myArgs.length === 2) {
   console.log('Argument found');
+} else if (myArgs.length > 1) {
+  console.log('Arguments found');
+} else {
+    console.log('No argument');
 }
