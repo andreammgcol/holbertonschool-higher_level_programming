038bec361e5ae4738596cf7ddf83c45101c67823
andreammgcol <amendez72@misena.edu.co>
2020-05-12 13:37:20 -0500

    fixing an unsolicited output

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,5 @@ const fs = require('fs');
 fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
   if (err) {
     console.log(err);
-  } else {
-    console.log(process.argv[3]);
   }
 });
