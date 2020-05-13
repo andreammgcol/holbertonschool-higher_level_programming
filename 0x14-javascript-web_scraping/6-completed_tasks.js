953176a2ce101b2cd62b12f7369901565b4c7cec
andreammgcol <amendez72@misena.edu.co>
2020-05-12 22:43:48 -0500

    adding task 6 finally

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const tasks = JSON.parse(body);
+    const result = {};
+    const report = [];
+
+    for (const task of tasks) {
+      if (task.completed) {
+        var userFound = report.find(element => element.id === task.userId);
+        if (!userFound) {
+          report.push({ id: task.userId, count: 1 });
+        } else {
+          userFound.count += 1;
+        }
+      }
+    }
+
+    for (const item of report) {
+      result[item.id] = item.count;
+    }
+
+    console.log(result);
+  }
+});
