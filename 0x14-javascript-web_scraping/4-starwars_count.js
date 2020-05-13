2695e514e1646e4a3e998e3a1291c704e1855bb9
andreammgcol <amendez72@misena.edu.co>
2020-05-12 19:44:35 -0500

    adding task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,23 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/';
+const url = process.argv[2];
 
 request(url, (err, response, body) => {
   if (err) {
     console.log(err);
   } else {
     const apiData = JSON.parse(body);
-    // console.log(data.title);
-    // console.log(JSON.parse(body).title);
-
     const films = apiData.results;
+    const ID = 'https://swapi-api.hbtn.io/api/people/18/';
+    let count = 0;
     for (let i = 0; i < films.length; i++) {
-       const characters = films[i].characters
-       for (let j = 0;  j < characters.length; j++){
-         console.log(`personaje: ${characters[j]}`);
-       }      
+      const characters = films[i].characters;
+      for (let j = 0; j < characters.length; j++) {
+        if (characters[j] === ID) {
+          count++;
+        }
+      }
     }
+    console.log(count);
   }
 });
